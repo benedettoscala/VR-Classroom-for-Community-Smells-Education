751a24a577ef8d2cc5ecb6cdf52314e87dd1034d
Benedetto Scala <100568040+benedettoscala@users.noreply.github.com>
2024-08-31 19:26:01 +0200

    Revert CloudThoughts.cs

--- Assets/Scripts/CloudThoughts.cs
+++ Assets/Scripts/CloudThoughts.cs
@@ -1,117 +1,201 @@
-﻿using UdonSharp;
+﻿
+using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC.SDKBase;
+using VRC.Udon;
 using TMPro;
 
 public class CloudThoughts : UdonSharpBehaviour
 {
-    [Header("UI Elements")]
     public RawImage cloud;
     public TextMeshProUGUI text;
-    public RawImage thinkingCloud;
-    public Image questionMark;
-    public Image dontCareMeme;
-    public Image jollyCooperation;
-
-    [Header("Emotion Faces")]
     public RawImage angryFace;
     public RawImage happyFace;
     public RawImage sadFace;
     public RawImage thinkingFace;
     public RawImage dizzyFace;
+
+    public RawImage thinkingCloud;
+
     public RawImage muteFace;
 
-    [Header("Movement Settings")]
+    public Image dontCareMeme;
+
+    public Image questionMark;
+
+    public Image jollyCooperation;
+
     public bool upAndDown = false;
-    public float speed = 2.0f;
-    public float height = 1.0f;
+
+    public float speed = 2.0f; // Velocità di movimento
+    public float height = 1.0f; // Altezza massima del movimento
 
     private Vector3 startPos;
-    private RawImage[] allFaces;
+
 
     void Start()
     {
-        startPos = transform.position;
-        allFaces = new RawImage[] { angryFace, happyFace, sadFace, thinkingFace, dizzyFace, muteFace };
-        ResetAllElements();
+        dontCareMeme.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+        startPos = transform.position; // Salva la posizione iniziale
     }
 
     void Update()
     {
-        FaceLocalPlayer();
-        if (upAndDown)
-        {
-            MoveUpAndDown();
-        }
-    }
-
-    private void FaceLocalPlayer()
-    {
+        //la cloud è sempre rivolta verso il player
         transform.LookAt(Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position);
-    }
-
-    private void MoveUpAndDown()
-    {
-        float newY = startPos.y + Mathf.Sin(Time.time * speed) * height;
-        transform.position = new Vector3(startPos.x, newY, startPos.z);
-    }
-
-    private void ResetAllElements()
-    {
-        foreach (var face in allFaces)
-        {
-            face.gameObject.SetActive(false);
+        if (upAndDown) {
+            //fai muovere la cloud su e giù
+            float newY = startPos.y + Mathf.Sin(Time.time * speed) * height;
+            transform.position = new Vector3(startPos.x, newY, startPos.z);
         }
+        
+    }
+
+    //make a thought
+    public void AngryThought()
+    {
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(true);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+        muteFace.gameObject.SetActive(false);
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
+    }
+
+    public void HappyThought()
+    {
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(true);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+        muteFace.gameObject.SetActive(false);
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
+    }
+
+    public void SadThought()
+    {
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(true);
+        thinkingFace.gameObject.SetActive(false);
+        muteFace.gameObject.SetActive(false);
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
+    }
+
+    public void ThinkingThought()
+    {
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(true);
+        muteFace.gameObject.SetActive(false);
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
+    }
+
+    public void NoThought()
+    {
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+        muteFace.gameObject.SetActive(false);
         thinkingCloud.gameObject.SetActive(false);
         cloud.gameObject.SetActive(false);
         questionMark.gameObject.SetActive(false);
-        dontCareMeme.gameObject.SetActive(false);
         jollyCooperation.gameObject.SetActive(false);
-        text.gameObject.SetActive(false);
     }
 
-    private void SetEmotionFace(RawImage faceToActivate)
+    public void DizzyThought()
     {
-        ResetAllElements();
-        faceToActivate.gameObject.SetActive(true);
+        dizzyFace.gameObject.SetActive(true);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+        muteFace.gameObject.SetActive(false);
         thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
     }
 
-    public void AngryThought() => SetEmotionFace(angryFace);
-    public void HappyThought() => SetEmotionFace(happyFace);
-    public void SadThought() => SetEmotionFace(sadFace);
-    public void ThinkingThought() => SetEmotionFace(thinkingFace);
-    public void DizzyThought() => SetEmotionFace(dizzyFace);
-    public void MuteThought() => SetEmotionFace(muteFace);
+    public void MuteThought()
+    {
+        muteFace.gameObject.SetActive(true);
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
 
-    public void NoThought() => ResetAllElements();
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(false);
+    }
+
 
     public void QuestionThought()
     {
-        ResetAllElements();
+        muteFace.gameObject.SetActive(false);
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+
         thinkingCloud.gameObject.SetActive(true);
         questionMark.gameObject.SetActive(true);
+        jollyCooperation.gameObject.SetActive(false);
     }
 
+    public void JollyCooperationThought()
+    {
+        muteFace.gameObject.SetActive(false);
+        dizzyFace.gameObject.SetActive(false);
+        angryFace.gameObject.SetActive(false);
+        happyFace.gameObject.SetActive(false);
+        sadFace.gameObject.SetActive(false);
+        thinkingFace.gameObject.SetActive(false);
+
+        thinkingCloud.gameObject.SetActive(true);
+        questionMark.gameObject.SetActive(false);
+        jollyCooperation.gameObject.SetActive(true);
+    }
+    
     public void setDontCareMeme(bool active)
     {
-        ResetAllElements();
+        //deactivate everything besides the dont care meme
         dontCareMeme.gameObject.SetActive(active);
     }
-
-    public void setJollyCooperation(bool active)
+    
+    public void setText(string t)
     {
-        ResetAllElements();
-        jollyCooperation.gameObject.SetActive(active);
+        
+        text.gameObject.SetActive(true);
+        text.text = t;
     }
 
-    public void setText(string message)
+    public void activateText()
     {
         text.gameObject.SetActive(true);
-        text.text = message;
     }
 
-    public void activateText() => text.gameObject.SetActive(true);
-    public void deactivateText() => text.gameObject.SetActive(false);
+    public void deactivateText()
+    {
+        text.gameObject.SetActive(false);
+    }
 }
